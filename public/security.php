77d54d6ea358e9729e19832f410fb2dd9bbcd27a
getblazeweb <admin@getblazeweb.com>
2026-02-27 20:54:21 -0500

    Update security.php
    Fixed some missing nav elements.

--- public/security.php
+++ public/security.php
@@ -138,8 +138,11 @@ $otpauth = $pendingSecret !== ''
             <div class="brand"><?php echo htmlspecialchars((string) config('app_name'), ENT_QUOTES, 'UTF-8'); ?></div>
             <nav class="topbar-actions">
                 <a href="/index.php" class="link">Dashboard</a>
+                <a href="/index.php?action=expiry" class="link">Expiry</a>
+                <a href="/index.php?action=domain_import" class="link">Import</a>
                 <a href="/security.php" class="link">Security</a>
                 <a href="/updater.php" class="link">Update</a>
+                <a href="/index.php" class="link">Help</a>
                 <a href="/logout.php" class="link">Logout</a>
             </nav>
         </div>
