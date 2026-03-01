8ad98c746666b70a0fe3b1855d3c97303cc13dff
getblazeweb <admin@getblazeweb.com>
2026-02-28 20:19:39 -0500

    Create current_verison.php

---
+++ current_verison.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * Current application version.
+ * Format: MAJOR.MINOR.PATCH (SemVer)
+ *
+ * MAJOR: Breaking changes, incompatible API or config changes
+ * MINOR: New features, backward compatible
+ * PATCH: Bug fixes, backward compatible
+ *
+ * Examples: 1.0.0 (initial release), 1.1.0 (new feature), 1.0.1 (bug fix), 2.0.0 (breaking)
+ */
+return '1.0.0';
